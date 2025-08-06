d72ed69c372848a86b2cd4a75d1671330614ede0
Anika Afrin Moumeta <anikaafrin3344@gmail.com>
2025-08-07 01:17:42 +0600

    If Touch detect Then Buzzer & LED Blink

--- sketch_may24a.ino
+++ sketch_may24a.ino
@@ -1,17 +1,56 @@
-int ledPin = 13;   
-int buzzerPin = 8;  
+#define TOUCH_PIN    T0  
+#define BUZZER_PIN   13 
+#define LED_PIN      12  
+
+const int TOUCH_THRESHOLD = 40;
+
+bool wasTouched = false;
+unsigned long previousMillis = 0;
+const unsigned long interval =  250;
+bool ledBuzzerState = false;         
 
 void setup() {
-  pinMode(ledPin, OUTPUT);     
-  pinMode(buzzerPin, OUTPUT);
+  Serial.begin(115200);
+  delay(1000); 
+
+  pinMode(BUZZER_PIN, OUTPUT);
+  pinMode(LED_PIN, OUTPUT);
+  digitalWrite(BUZZER_PIN, LOW);
+  digitalWrite(LED_PIN, LOW);
+
+  Serial.println("✅ System ready. Monitoring iron wire...");
 }
 
 void loop() {
-  digitalWrite(ledPin, HIGH);   
-  digitalWrite(buzzerPin, HIGH); 
-  delay(500);                 
+  int touchValue = touchRead(TOUCH_PIN);
+  bool isTouched = (touchValue < TOUCH_THRESHOLD);
 
-  digitalWrite(ledPin, LOW);    
-  digitalWrite(buzzerPin, LOW); 
-  delay(500);                  
+  unsigned long currentMillis = millis();
+
+  if (isTouched) {
+    if (!wasTouched) {
+      Serial.println("🚨 Someone Is In Your Area !!!⚠️");
+      previousMillis = currentMillis;
+      ledBuzzerState = true;
+      digitalWrite(BUZZER_PIN, HIGH);
+      digitalWrite(LED_PIN, HIGH);
+    } else {
+      if (currentMillis - previousMillis >= interval) {
+        previousMillis = currentMillis;
+        ledBuzzerState = !ledBuzzerState;
+
+        digitalWrite(BUZZER_PIN, ledBuzzerState ? HIGH : LOW);
+        digitalWrite(LED_PIN, ledBuzzerState ? HIGH : LOW);
+      }
+    }
+  } else {
+    if (wasTouched) {
+      Serial.println("✅ Safe, No One Here.");
+      Serial.println("🔕 Alarm OFF");
+      digitalWrite(BUZZER_PIN, LOW);
+      digitalWrite(LED_PIN, LOW);
+    }
+  }
+
+  wasTouched = isTouched;
 }
